04c9c217605ef63c5a6456f7c5e250bc2f0849e2
vk1679 <venkata.kurapati@accenture.com>
2026-09-07 15:49:52 +1200

    Initial project files

---
+++ src/main/java/com/nz/prep/PrepApplication.java
@@ -0,0 +1,12 @@
+package com.nz.prep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrepApplication.class, args);
+    }
+}
---
+++ src/test/java/com/nz/prep/PrepApplicationTests.java
@@ -0,0 +1,12 @@
+package com.nz.prep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
